4f43ab24b89e81beffb4219b19274089008ca6bb
Franco R <franrache@gmail.com>
2022-12-13 14:23:59 -0300

    ruleta funcionando

--- ruleta.js
+++ ruleta.js
@@ -1,9 +1,5 @@
 const url = "https://api.reelgood.com/v3.0/content/random?region=us&sources=netflix"
 
-function ratingenestrellas(pelicula.imdb_rating){
-   
-}
-
 function llamadaApi(){
     fetch(url)
     .then((resultado) => {
@@ -19,6 +15,6 @@ function llamadaApi(){
 
 function agregarALista(pelicula) {
     const lista = document.getElementById("cuadro_del_api")
-    lista.innerHTML = "<div class='cuadroapi'><p>"+"Nombre de la pelicula: "+pelicula.title+"<br></p><p>"+"Sinopsis: "+pelicula.overview+"</p><br><p>"+"Puntuacion segun IMDB: "+ratingenestrellas(pelicula.imdb_rating)+"</p><br></div>"
+    lista.innerHTML = "<div class='cuadroapi'><p>"+"Nombre de la pelicula: "+pelicula.title+"<br></p><p>"+"Sinopsis: "+pelicula.overview+"</p><br><p>"+"Puntuacion segun IMDB:" + pelicula.imdb_rating +"</p><br></div>"
 }
 
